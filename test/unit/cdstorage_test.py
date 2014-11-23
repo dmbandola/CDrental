a8e8134e660a995602082a4f077d30ebf12393bb
thundervoice <dmbbandola@gmail.com>
2014-11-23 11:48:26 +0800

    added test for cd_info

--- test/unit/cdstorage_test.py
+++ test/unit/cdstorage_test.py
@@ -7,3 +7,21 @@ class CDStorageTest(unittest.TestCase):
         cdstorage = CDStorage()
         self.assertEqual({}, cdstorage.cds)
         self.assertEqual(len(cdstorage.cds), 0)
+
+    def test_add_cd_to_storage(self):
+        cdstorage = CDStorage()
+        cd1 = CD(001, "XOXO", "Available")
+        cd2 = CD(002, "2014 S/S", "Available")
+
+        cdstorage.add_cd(cd1)
+        cdstorage.add_cd(cd2)
+        self.assertEqual(len(cdstorage.cds), 2)
+
+    def test_get_cd_info(self):
+        cdstorage = CDStorage()
+        cd1 = CD(001, "XOXO", "Available")
+        cdstorage.add_cd(cd1)
+        self.assertEqual(cdstorage.get_cd_info(001), cd1)
+
+if __name__ == '__main__':
+    unittest.main()
